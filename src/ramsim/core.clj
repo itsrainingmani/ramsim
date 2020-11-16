3dd63d8eef86c7918b3828a0f93b2b570a8bb928
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-11-16 17:24:28 -0500

    Add functions to wire up a gate

--- src/ramsim/core.clj
+++ src/ramsim/core.clj
@@ -11,5 +11,19 @@
                   :nand-gates [{:ins  [:a :b]
                                 :out :c}]})
 
+; update state v0
+; ---------------
 
+(def empty-state {:charge-map {} :nand-gates []})
 
+(defn charge [state wire]
+  (get-in state [:charge-map wire]))
+
+(defn charges [state wires]
+  (map (partial charge state) wires))
+
+(defn set-charge [state wire charge]
+  (assoc-in state [:charge-map wire] charge))
+
+(defn wire-nand-gate [state a b o]
+  (update state :nand-gates conj {:ins [a b] :out o}))
